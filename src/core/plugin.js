26f989aec993c3e3bf2f8697f23992cd705c71e9
Dean Gardiner <me@dgardiner.net>
2016-10-04 23:38:17 +1300

    Updated [manifest.json] to the new permissions structure, and updated the enable option to request the required permissions

--- src/core/plugin.js
+++ src/core/plugin.js
@@ -1,9 +1,11 @@
 import DestinationPlugin from 'eon.extension.framework/base/plugins/destination';
 
+import Manifest from '../../manifest.json';
+
 
 export class LastFmPlugin extends DestinationPlugin {
     constructor() {
-        super('lastfm', 'Last.fm');
+        super('lastfm', 'Last.fm', Manifest);
     }
 }
 
