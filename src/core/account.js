51e3a1f3dbbd91f525b9c2dae16ddd8906d01407
Dean Gardiner <me@dgardiner.net>
2017-09-04 13:31:30 +1200

    Removed the use of `Client.ready` in the [core/account] module

--- src/core/account.js
+++ src/core/account.js
@@ -6,21 +6,15 @@ import Plugin from './plugin';
 
 export default class Account {
     static refresh() {
-        // Ensure client has been initialized
-        return Client.ready.then(() =>
-            // Fetch account details
-            Client['user'].getInfo().then((account) => {
-                console.log('account:', account);
+        return Client['user'].getInfo().then((account) => {
+            // Store account details
+            Storage.putObject(Plugin.id + ':account', account);
 
-                // Store account details
-                Storage.putObject(Plugin.id + ':account', account);
-
-                return account;
-            }, (body, statusCode) => {
-                return Promise.reject(new Error(
-                    'Unable to retrieve account settings, response with status code ' + statusCode + ' returned'
-                ));
-            })
-        );
+            return account;
+        }, (body, statusCode) => {
+            return Promise.reject(new Error(
+                'Unable to retrieve account settings, response with status code ' + statusCode + ' returned'
+            ));
+        });
     }
 }
