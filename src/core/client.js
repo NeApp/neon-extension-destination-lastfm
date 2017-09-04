aa9892fb6b31024e88ebd17bfbc19d6cc80c5760
Dean Gardiner <me@dgardiner.net>
2017-09-04 14:01:53 +1200

    Client session will now be updated correctly on configuration changes

--- src/core/client.js
+++ src/core/client.js
@@ -2,19 +2,31 @@ import Storage from 'eon.extension.browser/storage';
 
 import {Client as LastFM} from '@fuzeman/lastfm/src/index';
 
+import Log from './logger';
 import Plugin from './plugin';
 
 
-var Client = new LastFM(
+const Client = new LastFM(
     '2c794d3b3415a2fb072f41fc8a8edcc7',
     '92a0a2adaf14f954e8d8999a9fb95524'
 );
 
-// Retrieve account token
-Client.ready = Storage.getObject(Plugin.id + ':session')
-    .then((session) => {
-        // Update client authorization
+function updateSession() {
+    // Retrieve session from storage
+    Storage.getObject(Plugin.id + ':session').then((session) => {
+        Log.debug('[%s] Session changed to: %o', window.location.href, session);
+
+        // Update client
         Client.session = session;
+
+        Log.debug('[%s] Client: %o', window.location.href, Client);
     });
+}
+
+// Update client if session changes
+Storage.onChanged(Plugin.id + ':session', updateSession);
+
+// Set initial client session
+updateSession();
 
 export default Client;
