dc295d2f587b2a5ee15d50001de90be7de32ba1c
Dean Gardiner <me@dgardiner.net>
2017-09-29 14:27:50 +1300

    Updated plugin construction

--- src/core/plugin.js
+++ src/core/plugin.js
@@ -1,11 +1,9 @@
 import DestinationPlugin from 'neon-extension-framework/base/plugins/destination';
 
-import Manifest from '../../module.json';
-
 
 export class LastFmPlugin extends DestinationPlugin {
     constructor() {
-        super('lastfm', Manifest);
+        super('lastfm');
     }
 }
 
