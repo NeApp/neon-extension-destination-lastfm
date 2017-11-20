d398e2b0339a1e16095237f7d1ca47cb42f1f089
Dean Gardiner <me@dgardiner.net>
2017-11-21 11:59:36 +1300

    Removed unused imports in the migrate service

--- src/services/migrate/index.js
+++ src/services/migrate/index.js
@@ -1,7 +1,6 @@
 import MigrateService from 'neon-extension-framework/services/migrate';
 import Plugin from 'neon-extension-destination-lastfm/core/plugin';
 import Registry from 'neon-extension-framework/core/registry';
-import {isDefined} from 'neon-extension-framework/core/helpers';
 
 
 export class LastFmMigrateService extends MigrateService {
